fdf783f86bc7ba974c55d709ff510d5d719c83d4
Ulistonee <ulistonee@gmail.com>
2026-03-16 09:10:23 +0500

    initial configuration

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      prettier,
+    },
+  },
+];
